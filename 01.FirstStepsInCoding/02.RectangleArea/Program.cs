4d95677a91fe0159276a3ae15bb2d76777c2bca7
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2020-09-16 23:53:43 +0300

    Add FirstStepsInCoding

---
+++ 01.FirstStepsInCoding/02.RectangleArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02.RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+
+            int area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
